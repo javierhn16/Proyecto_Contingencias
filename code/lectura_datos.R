a97b9eb8d187a9ea910fdb7920596eead8a3b094
javierhn16 <javierhdezn@gmail.com>
2024-06-16 23:52:24 -0600

    feat: limpia los nombres de las columnas

--- code/lectura_datos.R
+++ code/lectura_datos.R
@@ -10,12 +10,13 @@ qx_femenino <- read_excel('data/tavid2000-2150.xlsx', sheet = "Sexo_2_limpio") %
 base_empleados <- read_excel('data/Base de datos.xlsx')  
 
 # Transformaciones en la base de empleados
-# Se agrega columna "Edad"
-# Se cambia la columna "Sexo" a factores numéricos 
-base_empleados <- mutate(base_empleados,
-                         Edad = 2023 - as.numeric(format(`Fecha de nacimiento`, "%Y")),
-                         Sexo = as.numeric(recode(Sexo, 'M' = '0', 'F' = '1'))
-                         )
+base_empleados <- base_empleados %>% 
+  mutate(Edad = 2023 - as.numeric(format(`Fecha de nacimiento`, "%Y")),
+         Sexo = if_else(Sexo == 'M', 0, 1))
+
+# Limpia los nombres de la base_empleados
+colnames(base_empleados) <- c('id', 'fecha_nacimiento', 'sexo', 'edad')
+
 
 
 
