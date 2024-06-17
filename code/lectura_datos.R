7bcc16577ed121f4c1edbf129598b38906f2cdb5
javierhn16 <javierhdezn@gmail.com>
2024-06-16 23:25:23 -0600

    feat: agrega edad y sexo como factor en base_empleados

--- code/lectura_datos.R
+++ code/lectura_datos.R
@@ -1,7 +1,22 @@
 
 # Lectura de las tablas de mortalidad
-qx_masculino <- read_excel('data/tavid2000-2150.xlsx', sheet = "Sexo_1_limpio")
-qx_femenino <- read_excel('data/tavid2000-2150.xlsx', sheet = "Sexo_2_limpio")
+qx_masculino <- read_excel('data/tavid2000-2150.xlsx', sheet = "Sexo_1_limpio") %>%
+  mutate(across(everything(), as.numeric))
+
+qx_femenino <- read_excel('data/tavid2000-2150.xlsx', sheet = "Sexo_2_limpio") %>%
+  mutate(across(everything(), as.numeric))
 
 # Lectura de la base de datos con los empleados
-base_empleados <- read_excel('data/Base de datos.xlsx')
+base_empleados <- read_excel('data/Base de datos.xlsx')  
+
+# Transformaciones en la base de empleados
+# Se agrega columna "Edad"
+# Se cambia la columna "Sexo" a factores numéricos 
+base_empleados <- mutate(base_empleados,
+                         Edad = 2023 - as.numeric(format(`Fecha de nacimiento`, "%Y")),
+                         Sexo = as.numeric(recode(Sexo, 'M' = '0', 'F' = '1'))
+                         )
+
+
+
+
