214bc4e6b9026a571627d824987cd9070f7e0de4
javierhn16 <javierhdezn@gmail.com>
2024-06-23 15:22:35 -0600

    fix: corrige función para obtener los qx de la lista con las tablas de la SUPEN

--- code/modelo_estocastico.R
+++ code/modelo_estocastico.R
@@ -9,26 +9,25 @@
 #' @returns lista con la edad, sexo y las probabilidad de muerte de la persona
 obtener_qx <- function(edad, sexo){
   
-  if (sexo == 0) {
-    qx <- sapply(0:(64 - edad), function(j) as.numeric(qx_masculino[edad + 1 + j,  25 + j]))
+  if (sexo == 1) {
+    qx <- sapply(0:(64 - edad), function(j) as.numeric(tablas_supen$qx_masculino[edad + 1 + j,  25 + j]))
   } else {
-    qx <- sapply(0:(64 - edad), function(j) as.numeric(qx_femenino[edad + 1 + j,  25 + j]))
+    qx <- sapply(0:(64 - edad), function(j) as.numeric(tablas_supen$qx_femenino[edad + 1 + j,  25 + j]))
   }
-  return(list(edad = edad, sexo = sexo, probabilidad_muerte = qx))
+  return(list(edad = edad, sexo = sexo, qxs = qx))
 }
 
 # Se vectoriza la función obtener_qx
 obtener_qx_vect <- Vectorize(obtener_qx)
 
-
 # Función para realizar simulaciones --------------------------------------
-realizar_simulaciones <- function(data, n_simulaciones){
+realizar_simulaciones <- function(dataframe, n_simulaciones){
   
   # Crea un vector para guardar los resultados
   resultados <- vector('list', n_simulaciones)
   
   # Obtiene las edades y sexos únicos
-  combinaciones_unicas <- data %>%
+  combinaciones_unicas <- dataframe %>%
     select(sexo, edad) %>%
     distinct()
   
@@ -36,23 +35,41 @@ realizar_simulaciones <- function(data, n_simulaciones){
   qx_unicos <- t(obtener_qx_vect(combinaciones_unicas$edad, combinaciones_unicas$sexo))
   
   # Agrega la probabilidad de muerte a cada empleado
-  data <- merge(data, qx_unicos, by = c('edad', 'sexo'), all.x = TRUE)
+  dataframe <- merge(dataframe, qx_unicos, by = c('edad', 'sexo'), all.x = TRUE) %>%
+    select(id, sexo, edad, qxs)
   
-  # for (sim in seq_len(n_simulaciones)) {
-  #   
-  #   # Crea dataframe para la simulación
-  #   simulacion <- data %>%
-  #     select(id, edad, sexo) %>%
-  #     mutate(vivo = 1)  # Inicialmente, todos están vivos
-  #   
-  # }
+  n_row <- nrow(dataframe)
+  
+  # Realiza las simulaciones
+  for (sim in 1:n_simulaciones) {
+    
+    t <- proc.time() # ---------- Inicia cronómetro
+    # Inicialmente todos están vivos
+    poblacion <- rep(1, n_row)
+    suma_vp <- rep(0, n_row)
+    for (anno in 1:46){
+      v <- (1.03 / 1.07) ^ (anno - 1)
+      for (i in 1:n_row){
+        if (length(dataframe$qxs[[i]]) < anno){
+          print(i)
+          print('muerto por 1')
+          poblacion[i] <- 0
+        }
+        if (poblacion[i] == 1){
+          if (runif(1) <= dataframe$qxs[[i]][anno]) {
+            print(i)
+            print('muerto por 2')
+            poblacion[i] <- 0
+          }
+        }
+      }
+    }
+  }
+  print(proc.time() - t) # ---------- Termina cronómetro
+  return(dataframe)
 }
 
 
-t <- proc.time()
-prueba <- realizar_simulaciones(base_empleados, 1)
-proc.time() - t
-
-
-
-
+#t <- proc.time()
+(prueba <- realizar_simulaciones(base_empleados, 1))
+#proc.time() - t
