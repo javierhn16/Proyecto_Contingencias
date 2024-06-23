1d3450fc129f5c1964b6e8c9f58d7f9115fb245e
javierhn16 <javierhdezn@gmail.com>
2024-06-23 14:56:25 -0600

    feat: guarda las tablas de mortalidad en una lista y cambia factores de sexo de '0,1' a '1,2'

--- code/lectura_datos.R
+++ code/lectura_datos.R
@@ -1,10 +1,11 @@
 
-# Lectura de las tablas de mortalidad
-qx_masculino <- read_excel('data/tavid2000-2150.xlsx', sheet = "Sexo_1_limpio") %>%
-  mutate(across(everything(), as.numeric))
-
-qx_femenino <- read_excel('data/tavid2000-2150.xlsx', sheet = "Sexo_2_limpio") %>%
-  mutate(across(everything(), as.numeric))
+# Lectura de las tablas de mortalidad de la SUPEN
+tablas_supen <- list(
+  qx_masculino = read_excel('data/tavid2000-2150.xlsx', sheet = "Sexo_1_limpio") %>%
+    mutate(across(everything(), as.numeric)),
+  qx_femenino = read_excel('data/tavid2000-2150.xlsx', sheet = "Sexo_2_limpio") %>%
+    mutate(across(everything(), as.numeric))
+)
 
 # Lectura de la base de datos con los empleados
 base_empleados <- read_excel('data/Base de datos.xlsx')
@@ -12,7 +13,7 @@ base_empleados <- read_excel('data/Base de datos.xlsx')
 # Transformaciones en la base de empleados
 base_empleados <- base_empleados %>% 
   mutate(Edad = 2023 - as.numeric(format(`Fecha de nacimiento`, "%Y")),
-         Sexo = if_else(Sexo == 'M', 0, 1)) %>% 
+         Sexo = if_else(Sexo == 'M', 1, 2)) %>% 
   select(-`Fecha de nacimiento`)
 
 # Limpia los nombres de la base_empleados
