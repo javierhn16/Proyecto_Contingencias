f3ae4764ae8abfa77c976c6c0dc913ef61971c32
javierhn16 <javierhdezn@gmail.com>
2024-06-23 03:38:55 -0600

    feat: elimina columna de fecha de nacimiento

--- code/lectura_datos.R
+++ code/lectura_datos.R
@@ -7,15 +7,16 @@ qx_femenino <- read_excel('data/tavid2000-2150.xlsx', sheet = "Sexo_2_limpio") %
   mutate(across(everything(), as.numeric))
 
 # Lectura de la base de datos con los empleados
-base_empleados <- read_excel('data/Base de datos.xlsx')  
+base_empleados <- read_excel('data/Base de datos.xlsx')
 
 # Transformaciones en la base de empleados
 base_empleados <- base_empleados %>% 
   mutate(Edad = 2023 - as.numeric(format(`Fecha de nacimiento`, "%Y")),
-         Sexo = if_else(Sexo == 'M', 0, 1))
+         Sexo = if_else(Sexo == 'M', 0, 1)) %>% 
+  select(-`Fecha de nacimiento`)
 
 # Limpia los nombres de la base_empleados
-colnames(base_empleados) <- c('id', 'fecha_nacimiento', 'sexo', 'edad')
+colnames(base_empleados) <- c('id', 'sexo', 'edad')
 
 
 
