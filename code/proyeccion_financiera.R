774a19ec358f9424f149a04dbcb0cb423d67c4d1
Juan Pablo Morgan <juanpablomorgans@gmail.com>
2024-06-23 19:43:41 -0600

    fix: se arregla el problema de inflación en la funcion

--- code/proyeccion_financiera.R
+++ code/proyeccion_financiera.R
@@ -1,14 +1,18 @@
 # Esta función recibe la lista de la proyección demográfica de empleados activos.
 
-proyeccion_financiera <- function(lista){
+proyeccion_financiera <- function(lista, inflacion){
+  
+  # Calcula los factores de inflación
+  factores_inflacion <- (1 + inflacion)^(1:46)
+
   graf_proy_hombres <- data.frame(
     Annos = 2024:2069,  # Convertir los nombres de las columnas a numérico
-    Proyeccion =  t(lista[[3]]["Total", 2:47] * 5000000)
+    Proyeccion =  t(lista[[3]]["Total", 2:47] * 5000000) * factores_inflacion
   )
   
   graf_proy_mujeres <- data.frame(
     Annos = 2024:2069,  # Convertir los nombres de las columnas a numérico
-    Proyeccion = t(lista[[4]]["Total", 2:47] * 5000000)# Los valores de la fila
+    Proyeccion = t(lista[[4]]["Total", 2:47] * 5000000) * factores_inflacion
   )
   
   resultado <- list(graf_proy_hombres, graf_proy_mujeres)
