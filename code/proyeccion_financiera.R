d5ed53086d2453d541d94398cee4988bbd4602eb
Juan Pablo Morgan <juanpablomorgans@gamil.com>
2024-06-28 17:42:39 -0600

    fix: se corrige el return de la proyección financiera de activos

--- code/proyeccion_financiera.R
+++ code/proyeccion_financiera.R
@@ -7,7 +7,9 @@ proyeccion_financiera <- function(lista, inflacion){
 
   graf_proy <- data.frame(
     Annos = 2024:2069,  # Convertir los nombres de las columnas a numérico
-    Proyeccion = t((lista[[4]]["Total", 2:47] + lista[[3]]["Total", 2:47])  * 5000000) * factores_inflacion
+    Proyeccion_Total = t((lista[[4]]["Total", 2:47] + lista[[3]]["Total", 2:47])  * 5000000) * factores_inflacion,
+    Proyeccion_H =  t(lista[[3]]["Total", 2:47] * 5000000) * factores_inflacion,
+    Proyeccion_M = t(lista[[4]]["Total", 2:47] * 5000000) * factores_inflacion
   )
   resultado <- graf_proy
   return(resultado)
